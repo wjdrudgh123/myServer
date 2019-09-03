969787f1aa48564b01e3e3cb05c81cfe0d4bbda7
Jung-Jaeho <wjdrudgh123@naver.com>
2019-09-03 19:51:26 +0900

    del src

--- src/jsx/app.jsx
+++
@@ -1,8 +0,0 @@
-const React = require("react");
-const ReactDom = require("react-dom");
-const View = require("./view.jsx");
-
-ReactDom.render(
-    <View/>,
-    document.getElementById("container")
-)
